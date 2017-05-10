bb8e035448475f306860944efed9057184f90126
Unknown <gironafrancisco86@gmail.com>
2017-05-09 23:23:39 -0300

    Actualizacion
    recargar

--- funciones.php
+++ funciones.php
@@ -1,172 +1,6 @@
 <?php
-<<<<<<<
 
 /*Inicio de la validacion de la registracion*/
-=======
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// - INICIO - FUNCIONES LEO - XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-// Funcion que borra todas las Cookies
-// Devuelve la variable $_COOKIE
-function borrarCookies() {
-  if ($_COOKIE) {
-    $expira = time() - 3600;
-
-    foreach($_COOKIE as $id => $cookie) {
-      // unset($_COOKIE[$key]);
-      setcookie($id, '', $expira);
-    };
-  };
-  return $_COOKIE;
-};
-
-// Funcion que verifica segun las Cookies si el usuario esta logueado.
-// devuelve IDUSER o FALSE
-function chequeaCookieUsuario() {
-  // Verificamos si existe una cookie con el idUser creada anteriormente.
-  // Si existe, la utilizamos para loguear al usuario
-  $return = false;
-  if (isset($_COOKIE["idUser"])) {
-    // Cambiamos la variable a True si la subfuncion loguear usuario funciono sin problemas
-    $return = $_COOKIE["idUser"];
-  }
-  // Devolvemos el estado
-  return $return;
-};
-
-// Funcion que verifica segun las Cookies si el usuario esta logueado.
-// devuelve IDUSER o FALSE
-function chequeaSessionUsuario() {
-  // Verificamos si existe una cookie con el idUser creada anteriormente.
-  // Si existe, la utilizamos para loguear al usuario
-  $return = false;
-  if (isset($_SESSION["idUser"])) {
-    // Cambiamos la variable a True si la subfuncion loguear usuario funciono sin problemas
-    $return = $_SESSION["idUser"];
-  }
-  // Devolvemos el estado
-  return $return;
-};
-
-// Funcion que LOGUEA al usuario que se pase por parametro
-// Para esto crea una cookie con el valor del idUser, asi como tambien lo carga en la variable session
-function loguearUsuarioCookies($arrayUsuario,$recordarme) {
-  $return = false;
-  // Si devuelve algun valor es por que encontro al usuario guardado en la Cookie
-  if ($arrayUsuario) {
-    // Si la informacion del usuario es correcta,
-
-    // Lo logueamos a travez de la variable SESSION
-    foreach ($arrayUsuario as $id => $valor) {
-      $_SESSION[$id] = $valor;
-    };
-    if ($recordarme) {
-      // Guardamos el array con la informacion del usuario en las cookies
-      guardarCookiesUsuario($arrayUsuario,$recordarme);
-      // cambiamos la variable a True por que logueamos al usuario
-    };
-    $return = $arrayUsuario["idUser"];
-  };
-  // Devolvemos el estado
-  return $return;
-};
-
-// Busca el usuario pasado por parametro en el Archivo Json.
-// si lo encuentra devuelve el valor del Array del usuario, sino devuelve false
-function buscarPorIdUser($idUser) {
-  $todos = traerTodos();
-  foreach ($todos as $usuario) {
-    if ($usuario["idUser"] == $idUser) {
-      // Quitamos el valor de la contraseña
-      unset($usuario["contraseña"]);
-      // Devolvemos el array completo del usuario
-      return $usuario;
-    }
-  }
-  return false;
-};
-
-//Funcion que guarda las cookies del usuario en la pc
-function guardarCookiesUsuario($arrayUsuario,$recordarme) {
-  // Si la variable $recordame es True guarda la cookie 1 Mes
-  // sino cuando cierra el navegador la pierde.
-  if ($recordarme) {
-    $expira       = time() + (3600 *  24  * 30); // 1 Mes
-    //Guardamos el array del usuario en las cookies
-    foreach ($arrayUsuario as $id => $valor) {
-      setcookie($id, $valor, $expira);
-    };
-    // } else {
-    // //Guardamos el array del usuario en las cookies
-    // foreach ($arrayUsuario as $id => $valor) {
-    //   setcookie($id, $valor);
-    // };
-  };
-};
-
-//Funcion que LOGUEA al usuario que se pase por parametro
-//Para esto crea una cookie con el valor del idUser, asi como tambien lo carga en la variable session
-function loguearUsuario($idUser) {
-  $return = false;
-  //Verificamos si el usuario de la Cookie es valido.
-  $arrayUsuario = buscarPorIdUser($idUser);
-  $usuario      = $arrayUsuario["idUser"];
-  //Si devuelve algun valor es por que encontro al usuario guardado en la Cookie
-  if ($usuario) {
-    //Si la informacion del usuario es correcta,
-    //Lo logueamos a travez de la variable SESSION
-    $_SESSION["idUser"] = $usuario;
-    //Devolvemos el usuario logueado
-    $return = $usuario;
-  };
-  //Devolvemos el estado
-  return $return;
-};
-
-function desloguearUsuario($idUser) {
-  session_start();
-  unset($_SESSION[$idUser]);
-  unset($_SESSION);
-  // unset($_SESSION["nombre_cliente"]);
-  session_destroy();
-  // header("Location: index.php");
-  // exit;
-}
-
-//Trae todos los usuarios del Json.
-// generalmente se utiliza desde otras funciones.
-
-// Se usa la funcion de FRAN 
-              // function traerTodos() {
-              //   $archivo        = file_get_contents("cookies.json");
-              //   $usuariosJSON   = explode(PHP_EOL, $archivo);
-              //   array_pop($usuariosJSON);
-              //   $usuariosFinal  = [];
-              //   foreach($usuariosJSON as $json) {
-              //     $usuariosFinal[] = json_decode($json, true);
-              //   }
-              //   return $usuariosFinal;
-              // };
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// - FIN - FUNCIONES LEO - XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-
-
-
-
-
-
-
-
-
-
-
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// - INICIO - FUNCIONES FRAN - XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
->>>>>>>
 function validarInformacion($informacion){
   $errores=[];
   /* valido el nombre, elimino espacios que puedan venir por el post y luego chequeo que haya ingresado algo dentro del campo */
@@ -174,7 +8,6 @@ function validarInformacion($informacion){
   if (strlen($nombre) == 0) {
     $errores["nombre"]  = "Debe completar su nombre";
    }
-<<<<<<<
     /* valido el apellido, elimino espacios que puedan venir por el post y luego chequeo que haya ingresado algo dentro del campo */
 $apellido=trim($informacion["apellido"]);
 if (strlen($apellido) == 0) {
@@ -198,42 +31,17 @@ if(buscarPorUsuario($usuario) != false){
 $mail = trim($informacion["mail"]);
 if (strlen($mail) == 0 ) {
   $errores["mail"]="Debe completar el mail";
-=======
-  $apellido=trim($informacion["apellido"]);
-  if (strlen($apellido) == 0) {
-    $errores["apellido"] = "Debe completar su apellido";
->>>>>>>
   }
-  $telefono =trim($informacion["telefono"]);
-  if (strlen($telefono) == 0) {
-    $errores["telefono"]= "Debe completar su telefono";
+  elseif (! filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+    $errores["mail"]="Debe completar con un mail valido";
   }
-  $mail = trim($informacion["mail"]);
-  if (strlen($mail) == 0) {
-    $errores["mail"]="Debe completar el mail";
-    }
-    elseif (! filter_var($mail, FILTER_VALIDATE_EMAIL)) {
-      $errores["mail"]="Debe completar con un mail valido";
-    }
-    elseif (buscarPorMail($mail) != false) {
-      $errores["mail"]="El mail ya existe";
-    }
-    if ($informacion["password"]=="") {
-      $errores["password"] = "Llena la contraseña";
-    }
-    if($informacion["cpassword"]==""){
-    $errores["cpassword"] = "Debe confirmar su contraseña";
+  elseif (buscarPorMail($mail) != false) {
+    $errores["mail"]="El mail ya existe";
   }
-<<<<<<<
   /*Valido que el lo que entre entra en password y confirmacion de contraseña no sea vacio*/
   if ($informacion["password"]=="") {
     $errores["password"] = "Llena la contraseña";
-=======
-  if ($informacion["password"] != "" && $informacion["cpassword"]!== "" && $informacion["password"] !== $informacion["cpassword"]) {
-    $errores["password"] = "la contraseña debe ser igual a su confirmación, pruebe nuevamente.";
->>>>>>>
   }
-<<<<<<<
   if($informacion["cpassword"]==""){
   $errores["cpassword"] = "Debe confirmar su contraseña";
 }
@@ -245,48 +53,25 @@ return $errores;
 }
 /*funcion para guardar imagen, ingreso lo que viene en la funcion imagen y los errores encontrados previamente.
 */
-=======
-  return $errores;
-  }
->>>>>>>
 
-<<<<<<<
 function guardarImagen($imagenPefil, $errores){
   if ($_FILES[$imagenPefil]["error"] == UPLOAD_ERR_OK) {
     $nombre=$_FILES[$imagenPefil]["name"];
     $archivo=$_FILES[$imagenPefil]["tmp_name"];
     $ext = pathinfo("nombre", PATHINFO_EXTENSION);
-=======
-  function guardarImagen($imagenPefil, $errores){
-    if ($_FILES[$imagenPefil]["error"] == UPLOAD_ERR_OK) {
-      $nombre=$_FILES[$imagenPefil]["name"];
-      $archivo=$_FILES[$imagenPefil]["tmp_name"];
->>>>>>>
 
-<<<<<<<
     if ($ext === "jpg" || $ext === "jpeg" || $ext === "png") {
       $miArchivo = dirname(__FILE__);
       $miArchivo = $miArchivo . "/img/";
       $miArchivo = $miArchivo . $_POST["usuario"] . "." . $ext;
       move_uploaded_file($archivo, $miArchivo);
-=======
-      $ext = pathinfo("nombre", PATHINFO_EXTENSION);
-      if ($ext== "jpg" || $ext == "jpeg" || $ext == "png") {
-        $miArchivo = dirname(__FILE__);
-        $miArchivo = $miArchivo . "/img/";
-        $miArchivo = $miArchivo . $_POST["usuario"] . "." . $ext;
-        move_uploaded_file($archivo, $miArchivo);
-      }
-      else {
-        $errores["imgPerfil"] = "Sube tu foto de perfil";
-      }
-    }else {
-      $errores["imgPerfil"] = "No se pudo subir la foto";
->>>>>>>
     }
-    return $errores;
+    else {
+      $errores["imgPerfil"] = "Sube tu foto de perfil";
+    }
+  }else {
+    $errores["imgPerfil"] = "No se pudo subir la foto";
   }
-<<<<<<<
   return $errores;
 }
 
@@ -294,17 +79,10 @@ function guardarImagen($imagenPefil, $errores){
 function guardarUsuario($usuario){
   $json= json_encode($usuario);
   $json=$json . PHP_EOL;
-=======
 
->>>>>>>
+  file_put_contents("usuarios.json", $json, FILE_APPEND);
+}
 
-  function guardarUsuario($usuario){
-    $json= json_encode($usuario);
-    $json=$json . PHP_EOL;
-    file_put_contents("usuarios.json", $json, FILE_APPEND);
-  }
-
-<<<<<<<
 /*funcion para crear el usuario con los datos que vinieron por post y hasheamos la la clave previamente. Traigo nuevo ID*/
 function crearUsuario($datos) {
   $usuario =[
@@ -316,20 +94,7 @@ function crearUsuario($datos) {
     "password"=>password_hash($datos["password"], PASSWORD_DEFAULT)
   ];
   $usuario["id"] = traerNuevoId();
-=======
-  function crearUsuario($datos) {
-    $usuario =[
-      "nombre"=>$datos["nombre"],
-      "apellido"=>$datos["apellido"],
-      "usuario"=>$datos["usuario"],
-      "telefono"=>$datos["telefono"],
-      "mail"=>$datos["mail"],
-      "password"=>password_hash($datos["password"], PASSWORD_DEFAULT)
-    ];
-    $usuario["id"] = traerNuevoId();
->>>>>>>
 
-<<<<<<<
   return $usuario;
 }
 /*traigo datos del json*/
@@ -340,44 +105,26 @@ function traerTodos() {
   $usuariosFinal = [];
   foreach($usuariosJSON as $json) {
     $usuariosFinal[] = json_decode($json, true);
-=======
-    return $usuario;
   }
 
-  function traerTodos() {
-    $archivo = file_get_contents("usuarios.json");
-    $usuariosJSON = explode(PHP_EOL, $archivo);
-    array_pop($usuariosJSON);
-    $usuariosFinal = [];
-    foreach($usuariosJSON as $json) {
-      $usuariosFinal[] = json_decode($json, true);
-    }
-    return $usuariosFinal;
->>>>>>>
-  }
-
-<<<<<<<
   return $usuariosFinal;
 }
 
 /*Con esta funcion asigno nuevo ID, el cual se utiliza a la hora de la creacion del usuario*/
 function traerNuevoId() {
   $usuarios = traerTodos();
-=======
 
->>>>>>>
+  if (count($usuarios) == 0) {
+    return 1;
+  }
 
+  $elUltimo = array_pop($usuarios);
 
-  function traerNuevoId() {
-    $usuarios = traerTodos();
+  $id = $elUltimo["id"];
 
-    if (count($usuarios) == 0) {
-      return 1;
-    }
+  return $id + 1;
+}
 
-    $elUltimo = array_pop($usuarios);
-
-<<<<<<<
 /*buscar por usuario*/
 
 function buscarPorUsuario($usuario){
@@ -393,51 +140,13 @@ return false;
 /*Funcion buscar por mail, esta nos sirve para verificar si el mail ingresado no se repite en nuestra base*/
 function buscarPorMail($mail) {
   $todos = traerTodos();
-=======
-    $id = $elUltimo["id"];
->>>>>>>
 
-    return $id + 1;
-  }
-
-  function buscarPorMail($mail) {
-    $todos = traerTodos();
-
-    foreach ($todos as $usuario) {
-      if ($usuario["mail"] == $mail) {
-        return $usuario;
-      }
+  foreach ($todos as $usuario) {
+    if ($usuario["mail"] == $mail) {
+      return $usuario;
     }
-
-    return false;
   }
-  // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-  // - FIN - FUNCIONES FRAN - XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-  // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-
-
-
-
-
-
-
-
-
-
-
-  // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-  // - INICIO - FUNCIONES ANDRES - XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-  // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-
-
-
-
-
-
-
-  // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-  // - FIN - FUNCIONES ANDRES - XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-  // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-?>
+  return false;
+}
+ ?>
--- header.php
+++ header.php
@@ -1,72 +1,10 @@
 <?php
-  // Pagina de funciones
-  require_once("funciones.php");
-
-  // iniciamos la session
-  session_start();
-
-  if ($_POST) {
-    // - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR
-    // - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR
-    $recordarme = false;
-    if (isset($_POST["recordarme"])) {
-      $recordarme = true;
-    };
-
-    if (isset($_POST["logIn"])) {
-      $usuarioForzado = "infoleod";
-      $arrayUsuario = buscarPorIdUser($usuarioForzado);
-      if ($arrayUsuario) {
-        // Si el usuario quieren que lo recuerden guardamos las cookies
-        $usuario = loguearUsuarioCookies($arrayUsuario,$recordarme);
-
-        header("Location:index.php");exit;
-      };
-    };
-    // - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR
-    // - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR - BORRAR
+  $variableTodoOk = 1;
 
 
-    if (isset($_POST["logoff"])) {
 
-      borrarCookies();
-      unset($_POST);
-      unset($_SESSION);
-      unset($_COOKIE);
 
-      session_destroy();
 
-      header("Location:index.php");exit;
-    };
-  };
-
-  // Variable que define el estado de los links de login
-  $usuarioLogueado = false;
-  // Llamamos a la funcion que valida si existe alguna cookie con nombre idUser
-  // Si lo encuentra devuelve el idUser, sino devuelve False
-  $idUser = chequeaCookieUsuario();
-
-  if ($idUser) {
-    //Buscamos que exista en la base de datos
-    $arrayUsuario = buscarPorIdUser($idUser);
-
-    if ($arrayUsuario) {
-      // La funcion loguearUsuarioCookie dentro valida si existe ese valor en la base de datos
-      $recordarme = true;
-      $usuarioLogueado = loguearUsuarioCookies($arrayUsuario,$recordarme);
-
-      if ($usuarioLogueado) {
-        $usuarioLogueado = "Hola " . $usuarioLogueado;
-      };
-    };
-  } else {
-    // Verificamos si tiene session activa, si es asi lo logueamos
-    $usuarioLogueado = chequeaSessionUsuario();
-
-    if ($usuarioLogueado) {
-      $usuarioLogueado = "Hola " . $usuarioLogueado;
-    };
-  };
 
 ?>
 
@@ -78,46 +16,13 @@
     <title>ZOOMarket</title>
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
   </head>
   <body>
     <!-- INICIO HEADER -->
     <header class="header">
-      <!-- INICIO Login -->
-      <div class="header-div-login">
-        <div class="header-div">
-          <!-- <div class="header-div-login-links">
-            <a href="formulario.php">Registrarse</a>
-            <a href="login.php">LogIn</a>
-          </div> -->
-          <?php
-          // Si el usuario esta logueado lo saludamos
-          // Si no le mostramos los datos del logi n
-          if ($usuarioLogueado) { ?>
-            <div class="header-div-login-links">
-              <form class="header-div-login-links-logoff" action="index.php" method="post">
-                <input type="submit" name="logoff" value="desloguearse">
-                <input class="header-div-login-icons" type="image" name="carrito" src="images/header-carrito.png" border="0" alt="Submit">
-                <input class="header-div-login-icons" type="image" name="miCuenta" src="images/header-avatar.png" border="0" alt="Submit">
-              </form>
-              <a href="#"><?=$usuarioLogueado?></a>
-            </div>
-          <?PHP } else { ?>
-            <form class="borrar-solo-pruebas" action="index.php" method="post">
-              <input type="submit" name="logIn" value="Loguear usuario">
-              <input type="checkbox" name="recordarme" value="true">Recordarme
-            </form>
-            <div class="header-div-login-links">
-              <a href="formulario.php">Registrarse</a>
-              <a href="login.php">LogIn</a>
-            </div>
-          <?PHP }; ?>
-
-
-        </div>
+      <div class="header-barra">
+        <img src="images/barra-horizontal.png" alt="barra">
       </div>
-      <!-- FIN Login -->
       <div class="header-div">
         <!-- INICIO Logo -->
         <div class="header-div-logo">
@@ -139,57 +44,61 @@
         </div>
         <!-- FIN Buscar -->
 
+        <!-- INICIO Menu -->
+        <div class="header-div-menu">
+          <nav>
+            <ul class="header-div-menu-icon">
+              <li class="header-div-menu-icon-li" >
+                <!-- ICONO De menu en Mobile -->
+                <a href="#">
+                  <img class="header-div-logo-mobile-menu" src="images/menu-icon.png" alt="menu-icon">
+                </a>
+
+                <!-- SubMenu con Lista desplegable -->
+                <ul class="header-div-menu-lista">
+                  <li><a class="header-div-menu-lista-login" href="login.php">LogIn</a></li>
+                  <li><a class="header-div-menu-lista-login" href="formulario.php">Registrarse</a></li>
+
+                  <!-- Productos -->
+                  <li>
+                    <a href="#">Productos</a>
+                    <ul class="header-div-menu-lista-principal">
+                      <li>
+                        <a href="#">Perros</a>
+                        <ul class="header-div-menu-lista-secundario">
+                          <li><a href="#">Cuchas</a></li>
+                          <li><a href="#">Juguetes</a></li>
+                          <li><a href="#">Comida</a></li>
+                          <li><a href="#">Peluqueria</a></li>
+                          <li><a href="#">Otros</a></li>
+                        </ul>
+                      </li>
+                      <li><a href="#">Gatos</a></li>
+                      <li><a href="#">Aves</a></li>
+                      <li><a href="#">Exoticos</a></li>
+                      <li><a href="#">Otros</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Ofertas</a></li>
+                  <li><a href="#">Top 10</a></li>
+                  <li><a href="#">Comprar</a></li>
+                  <li><a href="#">Vender</a></li>
+                </ul>
+              </li>
+            </ul>
+
+
+          </nav>
+        </div>
+        <!-- FIN Menu -->
+
+        <!-- INICIO Login -->
+        <div class="header-div-login">
+          <a href="login.php">LogIn</a>
+          <a href="formulario.php">Registrarse</a>
+        </div>
+        <!-- FIN Login -->
       </div>
-
-      <!-- INICIO Menu -->
-      <div class="header-div-menu">
-        <div class="header-div">
-        <nav>
-          <ul class="header-div-menu-icon">
-            <li class="header-div-menu-icon-li" >
-              <!-- ICONO De menu en Mobile -->
-              <a href="#">
-                <img class="header-div-logo-mobile-menu" src="images/menu-icon.png" alt="menu-icon">
-              </a>
-
-              <!-- SubMenu con Lista desplegable -->
-              <ul class="header-div-menu-lista">
-                <li><a class="header-div-menu-lista-login" href="login.php">LogIn</a></li>
-                <li><a class="header-div-menu-lista-login" href="formulario.php">Registrarse</a></li>
-
-                <!-- Productos -->
-                <li>
-                  <a href="#">Productos</a>
-                  <ul class="header-div-menu-lista-principal">
-                    <li>
-                      <a href="#">Perros</a>
-                      <ul class="header-div-menu-lista-secundario">
-                        <li><a href="#">Cuchas</a></li>
-                        <li><a href="#">Juguetes</a></li>
-                        <li><a href="#">Comida</a></li>
-                        <li><a href="#">Peluqueria</a></li>
-                        <li><a href="#">Otros</a></li>
-                      </ul>
-                    </li>
-                    <li><a href="#">Gatos</a></li>
-                    <li><a href="#">Aves</a></li>
-                    <li><a href="#">Exoticos</a></li>
-                    <li><a href="#">Otros</a></li>
-                  </ul>
-                </li>
-                <li><a href="#">Ofertas</a></li>
-                <li><a href="#">Top 10</a></li>
-                <li><a href="#">Comprar</a></li>
-                <li><a href="#">Vender</a></li>
-              </ul>
-            </li>
-          </ul>
-
-
-        </nav>
-      </div>
-      </div>
-      <!-- FIN Menu -->
     </header>
     <!-- FIN HEADER -->
 
